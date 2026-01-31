d51a4d8d24590db9bf65e8fd2aabaf7cb11c2195
hanzlashah <hanzlah013@gmail.com>
2026-01-31 05:27:38 +0500

    basic code of c language

---
+++ comment.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    //this is the single line comment
+//    printf("Hello World \n"); 
+/*
+This is the multi
+line comment
+*/
+    printf("Hello World \n");
+    return 0 ;
+}
